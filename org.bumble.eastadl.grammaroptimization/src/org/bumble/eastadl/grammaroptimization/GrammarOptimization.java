fae54b3f2a04bb99a6cce1abc167fba746abf6fb
wzhangasj <weixing.zhang@gu.se>
2021-05-04 21:33:10 +0200

    add java project for optimizing xtext grammar, automatically removing brackets already can work

---
+++ org.bumble.eastadl.grammaroptimization/src/org/bumble/eastadl/grammaroptimization/GrammarOptimization.java
@@ -0,0 +1,160 @@
+package org.bumble.eastadl.grammaroptimization;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class GrammarOptimization {	
+
+	/**
+	 * Main method (entry method)
+	 */
+    public static void main(String[] args) {    	
+    	GrammarOptimization optimizer = new GrammarOptimization();
+    	
+    	// test code
+    	//optimizer.test();
+    	
+    	// real optimization
+    	//optimizer.OptimizeInLIne();
+    	optimizer.OptimizeAsAWhole();
+    }
+    
+    /**
+     * Optimize xtext grammar as a whole
+     */
+    public void OptimizeAsAWhole() {
+		String fileName = "D:\\\\Git\\\\Git_Local\\\\east-adl-simplified\\\\org.bumble.eastadl.simplified\\\\src\\\\org\\\\bumble\\\\eastadl\\\\simplified\\\\MyDsl.xtext";
+    	File file = new File(fileName);
+		
+		try {
+			// read whole content from xtext file
+			String strOrigin = IOHelper.readFile(file);
+			
+			// optimize grammar in string-level
+			String strProcessed = Process(strOrigin);
+			
+			// write the optimized grammar back into the xtext file
+			IOHelper.saveFile(file, strProcessed);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+    }
+    
+    /**
+     * Optimize xtext grammar line by line
+     */
+    public void OptimizeInLIne() {
+    	// get file path
+    	String fileName = "D:\\Git\\Git_Local\\east-adl-simplified\\org.bumble.eastadl.simplified\\src\\org\\bumble\\eastadl\\simplified\\MyDsl.xtext";
+    	File file = new File(fileName);
+    	List<String> listString = new ArrayList<String>();
+    	
+    	try {
+			FileInputStream fis = new FileInputStream(file);
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader br = new BufferedReader(isr);
+			
+			// read and process string line by line
+			try {
+				String strSource, strTarget;
+				while ((strSource = br.readLine()) != null)
+				{
+					// remove '{' and '}' from input string
+					strTarget = Process(strSource);
+					
+					// put the processed string into list
+					listString.add(strTarget);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if (listString.isEmpty())
+				return;
+			
+			// write the processed strings into the file (cover the origin text)
+			BufferedWriter writer;
+			try {
+				writer = new BufferedWriter(new FileWriter(fileName));
+				
+				for (String strTemp:listString){
+		            try {
+						writer.write(strTemp + "\r\n");
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		        }
+				
+				writer.flush();
+				writer.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  	
+    }
+    
+    /**
+     * Specific optimization operation
+     */
+    public String Process(String strInput) {
+    	if (null == strInput || strInput.isEmpty())
+    		return "";
+    	
+    	String strOutput = "";
+    		
+    	// convert tab symbol to four whitespace
+    	Pattern replace1 = Pattern.compile("\t");
+    	Matcher matcher1 = replace1.matcher(strInput);
+    	String strTemp1 = matcher1.replaceAll("\s\s\s\s").toString();
+    	
+    	// remove '{' and '}' from input string
+    	String regex1 = "([\'])([{])([\'])";
+    	String strTemp2 = strTemp1.replaceAll(regex1, "\s");
+    	String regex2 = "([\'])([}])([\'])";
+    	strOutput = strTemp2.replaceAll(regex2, "\s");
+    	
+    	return strOutput;
+    }
+    
+    /**
+     * test code
+     */
+    public void test() {
+    	String EXAMPLE_TEST = " text	'{'";
+    	
+    	int nLine = 0;
+    	
+    	// print the origin text
+    	System.out.print(Integer.toString(nLine) + EXAMPLE_TEST + "\n");
+    	nLine++;
+    	
+    	// print the text after replacing tab symbol into whitespace
+    	Pattern replace1 = Pattern.compile("\t");
+        Matcher matcher1 = replace1.matcher(EXAMPLE_TEST);
+        System.out.print(Integer.toString(nLine) + matcher1.replaceAll("\s\s\s\s").toString() + "\n");        
+        nLine++;       
+        
+        // print the text after removing '{'
+        String regex = "([\'])([{])([\'])";
+        System.out.print(Integer.toString(nLine));
+        System.out.println(EXAMPLE_TEST.replaceAll(regex, "\s"));
+        nLine++;
+    }
+}
---
+++ org.bumble.eastadl.grammaroptimization/src/org/bumble/eastadl/grammaroptimization/IOHelper.java
@@ -0,0 +1,35 @@
+package org.bumble.eastadl.grammaroptimization;
+
+import java.io.Reader;
+import java.io.Writer;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class IOHelper {
+ 
+	public static void copy(Reader in,Writer out) throws IOException {
+		int c = -1;
+		while((c = in.read()) != -1) {
+			out.write(c);
+		}
+	}
+	
+	public static String readFile(File file) throws IOException {
+		if (file != null && file.canRead()){
+			Reader in = new FileReader(file);
+			StringWriter out = new StringWriter();
+			copy(in,out);
+			return out.toString();
+		}
+		return "";
+	}
+	
+	public static void saveFile(File file, String content) throws IOException {
+		Writer writer = new FileWriter(file);
+		writer.write(content);
+		writer.close();
+	}	
+}
