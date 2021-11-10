240f137a894449cb5c342b097cb6f8307dda0dce
wzhangasj <weixing.zhang@gu.se>
2021-11-10 21:45:18 +0100

    Move test case data source to resource
    In previous version, the test data was read from an absolute path
    which may not work for other contributors, so now move the data source
    to resource, i.e. dynamically get the path of current path and caculat
    e the texts file, and then read the contents inside.

---
+++ tests/org.bumble.eastadl.simplified.tests/src/org/bumble/eastadl/simplified/tests/FileOperateHelper.java
@@ -0,0 +1,62 @@
+package org.bumble.eastadl.simplified.tests;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FileOperateHelper {
+	private static final String ECLIPSE_WORKSPACE_PATH = "(.*\\\\east-adl-simplified)";
+	private static final String TEST_PROJECT_NAME = "org.bumble.eastadl.simplified.tests";
+	private static final String EXAMPLE_PROJECT_NAME = "org.bumble.eastadl.simplified.examples";
+	
+	public static String getProjectAbsolutePath() {
+		String strOutput = "";
+		
+		// Get absolute of current project
+		Path path = FileSystems.getDefault().getPath("");
+		String directoryName = path.toAbsolutePath().toString();
+
+		// Get Eclipse workpsace path
+		Pattern pattern = Pattern.compile(ECLIPSE_WORKSPACE_PATH);
+		String strEclipseWorkspace = getTargetString(directoryName, pattern);
+
+		// Construct name string of xtext text file
+		strOutput = strEclipseWorkspace + "\\tests\\" + TEST_PROJECT_NAME + "\\testsources\\";
+		
+		return strOutput;
+	}
+	
+	public static String getRootDirectoryAbsolutePath() {
+		String strOutput = "";
+		
+		// Get absolute of current project
+		Path path = FileSystems.getDefault().getPath("");
+		String directoryName = path.toAbsolutePath().toString();
+
+		// Get Eclipse workpsace path
+		Pattern pattern = Pattern.compile(ECLIPSE_WORKSPACE_PATH);
+		String strEclipseWorkspace = getTargetString(directoryName, pattern);
+
+		// Construct name string of xtext text file
+		strOutput = strEclipseWorkspace + "\\examples\\" + EXAMPLE_PROJECT_NAME + "\\";
+		
+		return strOutput;
+	}
+	
+	/**
+	 * Get the second matching group of the regex in a specific string
+	 */
+	private static String getTargetString(String strOrigin, Pattern pattern) {
+		if (null == strOrigin || null == pattern)
+			return null;
+
+		Matcher matcher = pattern.matcher(strOrigin);
+
+		if (matcher.find()) {
+			return matcher.group(1);
+		}
+
+		return null;
+	}
+}
